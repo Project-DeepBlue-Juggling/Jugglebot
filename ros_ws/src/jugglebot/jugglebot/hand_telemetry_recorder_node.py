4b15d245402ef347c8d6d3748e86041b11cd28c9
Jugleer <71805544+Jugleer@users.noreply.github.com>
2024-07-25 15:01:41 +1000

    Record commanded data as well
    Still need to improve the way the timestamps are handled, as commanded trajectories always start at time = 0

--- ros_ws/src/jugglebot/jugglebot/hand_telemetry_recorder_node.py
+++ ros_ws/src/jugglebot/jugglebot/hand_telemetry_recorder_node.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Trigger
-from jugglebot_interfaces.msg import HandTelemetryMessage
+from jugglebot_interfaces.msg import HandTelemetryMessage, HandTrajectoryPointMessage
 import csv
 import os
 
@@ -17,7 +17,12 @@ class HandTelemetryRecorderNode(Node):
         # Set up a subscriber to receive the telemetry data
         self.telemetry_subscription = self.create_subscription(HandTelemetryMessage, 'hand_telemetry', self.telemetry_callback, 10)
 
-        self.data = []
+        # Set up a subscriber to receive the commanded data
+        self.trajectory_subscription = self.create_subscription(HandTrajectoryPointMessage, 'hand_trajectory', self.trajectory_callback, 10)
+
+        # Initialize lists to store the data
+        self.cmd_data = []
+        self.telemetry_data = []
 
     def telemetry_callback(self, msg):
         # Extract the telemetry data from the message
@@ -32,18 +37,45 @@ class HandTelemetryRecorderNode(Node):
         # self.get_logger().info(f"Received telemetry: {position}, {velocity}")
 
         # Append the telemetry data to the data list
-        self.data.append([timestamp, position, velocity])
+        self.telemetry_data.append([timestamp, position, velocity])
+
+    def trajectory_callback(self, msg):
+        # Extract the trajectory data from the message
+        time = msg.time
+        pos = msg.pos
+        vel = msg.vel
+        tor = msg.tor
+
+        # Log the trajectory data
+        # self.get_logger().info(f"Received trajectory: {time}, {pos}, {vel}, {tor}")
+
+        # Append the trajectory data to the data list
+        self.cmd_data.append([time, pos, vel, tor])
 
     def save_data(self):
         # Define the file path
         desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
         file_path = os.path.join(desktop_path, 'hand_telemetry.csv')
 
-        # Write the data to the file
+        # Extract the telemetry and cmd data
+        telemetry_data = self.telemetry_data
+        cmd_data = self.cmd_data
+
+        # Find the length of the longer dataset
+        telemetry_length = max(len(telemetry_data), len(cmd_data))
+
+        # Pad the shorter dataset with None to ensure both datasets are the same length
+        cmd_data.extend([[None, None, None, None]] * (telemetry_length - len(cmd_data)))
+        telemetry_data.extend([[None, None, None]] * (telemetry_length - len(telemetry_data)))
+
+        # Write the data to the file so that all the data is saved into separate columns
         with open(file_path, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Timestamp', 'Position', 'Velocity'])
-            writer.writerows(self.data)
+            writer.writerow(['Time_telemetry', 'Position_telemetry', 'Velocity_telemetry', 
+                             'Time_cmd', 'Position_cmd', 'Velocity_cmd', 'Torque_cmd'])
+        
+            for t_data, c_data in zip(telemetry_data, cmd_data):
+                writer.writerow(t_data + c_data)
 
         # Log the file path
         self.get_logger().info(f"Data saved to: {file_path}")
