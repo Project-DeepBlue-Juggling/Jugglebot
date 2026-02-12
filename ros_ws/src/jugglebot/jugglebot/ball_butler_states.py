53e22144827b0169290b3598401f1bd082ec5dd7
Jugleer <71805544+Jugleer@users.noreply.github.com>
2026-02-12 17:49:16 +1100

    Add 'CHECKING_BALL' State

--- ros_ws/src/jugglebot/jugglebot/ball_butler_states.py
+++ ros_ws/src/jugglebot/jugglebot/ball_butler_states.py
@@ -1,10 +1,11 @@
 from enum import IntEnum
 
 class BallButlerStates(IntEnum):
-    BOOT        = 0
-    IDLE        = 1
-    TRACKING    = 2
-    THROWING    = 3
-    RELOADING   = 4
-    CALIBRATING = 5
-    ERROR       = 127
+    BOOT          = 0  # Initializing
+    IDLE          = 1  # Ready to track/throw. Note that BB may not be calibrated at this stage, as the calibration is a ROS process
+    TRACKING      = 2  # Actively tracking some target, but not yet throwing
+    THROWING      = 3  # Actively throwing a ball
+    RELOADING     = 4  # Moving Ball Butler through its reload sequence
+    CALIBRATING   = 5  # Calibrating Ball Butler's position in the world frame, using the mocap data
+    CHECKING_BALL = 6  # Checking that a ball has, in fact, been removed from the hand
+    ERROR         = 127
